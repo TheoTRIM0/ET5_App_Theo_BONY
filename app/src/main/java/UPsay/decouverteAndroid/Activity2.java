b0b9988638696f43abef6730244bf37e9e8add2a
Theo <btheo91@orange.fr>
2025-10-17 15:52:23 +0200

    feat: Add second activity to get user input
    Introduces a new activity, `Activity2`, which allows the user to enter text. This text is then returned to and displayed on the `MainActivity`.
    
    Specific changes:
    - Add `Activity2.java` and its corresponding layout file `activity_2.xml`.
    - Implement `ActivityResultLauncher` in `MainActivity` to start `Activity2` and handle the returned text.
    - Update `MainActivity`'s layout to `ConstraintLayout`, adding a button to launch `Activity2` and a `TextView` to display the result.
    - Add new string resources for the new UI elements.

---
+++ app/src/main/java/UPsay/decouverteAndroid/Activity2.java
@@ -0,0 +1,39 @@
+package UPsay.decouverteAndroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+    public static final String EXTRA = "message";
+    EditText monTexte;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void Revenir(View view){
+        monTexte = findViewById(R.id.textInputEditText);
+        String message = monTexte.getText().toString();
+
+        Intent messageIntent = new Intent();
+        messageIntent.putExtra(EXTRA, message);
+        setResult(Activity.RESULT_OK, messageIntent);
+        finish();
+    }
+}
--- app/src/main/java/UPsay/decouverteAndroid/MainActivity.java
+++ app/src/main/java/UPsay/decouverteAndroid/MainActivity.java
@@ -2,19 +2,22 @@ package UPsay.decouverteAndroid;
 
 import static java.security.AccessController.getContext;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityResultLauncher<Intent> lanceActivity2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +28,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        lanceActivity2 = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == MainActivity.RESULT_OK) {
+                Intent data = result.getData();
+                if (data != null) {
+                    String message = data.getStringExtra(Activity2.EXTRA);
+                    TextView textView = findViewById(R.id.textView1);
+                    textView.setText(message);
+                }
+            }
+        });
     }
 
     public void dessin(View view){
         TextView editText = (TextView) findViewById(R.id.monTexte);
         editText.setText("YES !!!");
         setContentView(R.layout.gestionaire2);
+    }
+
+    public void lanceActivite2(View view){
+        Intent intent = new Intent(this, Activity2.class);
+        lanceActivity2.launch(intent);
     }
 
     public void on_off(View view){
